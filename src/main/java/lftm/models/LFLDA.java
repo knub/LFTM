3a1a206ad917c85965204c85a3a730c15a62c4e1
Stefan Bunk <stefan.bunk@googlemail.com>
2016-07-19 07:54:27 +0200

    Better indentation

--- src/main/java/lftm/models/LFLDA.java
+++ src/main/java/lftm/models/LFLDA.java
@@ -244,37 +244,29 @@ public class LFLDA
         System.out.println("Reading word vectors from word-vectors file " + pathToWordVectorsFile
                 + "...");
 
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(pathToWordVectorsFile));
-            String[] elements = br.readLine().trim().split("\\s+");
-            vectorSize = elements.length - 1;
-            wordVectors = new double[vocabularySize][vectorSize];
-            String word = elements[0];
+        BufferedReader br = new BufferedReader(new FileReader(pathToWordVectorsFile));
+        String[] elements = br.readLine().trim().split("\\s+");
+        vectorSize = elements.length - 1;
+        wordVectors = new double[vocabularySize][vectorSize];
+        String word = elements[0];
+        if (word2IdVocabulary.containsKey(word)) {
+            for (int j = 0; j < vectorSize; j++) {
+                wordVectors[word2IdVocabulary.get(word)][j] = new Double(elements[j + 1]);
+            }
+        }
+        for (String line; (line = br.readLine()) != null;) {
+            elements = line.trim().split("\\s+");
+            word = elements[0];
             if (word2IdVocabulary.containsKey(word)) {
                 for (int j = 0; j < vectorSize; j++) {
                     wordVectors[word2IdVocabulary.get(word)][j] = new Double(elements[j + 1]);
                 }
             }
-            for (String line; (line = br.readLine()) != null;) {
-                elements = line.trim().split("\\s+");
-                word = elements[0];
-                if (word2IdVocabulary.containsKey(word)) {
-                    for (int j = 0; j < vectorSize; j++) {
-                        wordVectors[word2IdVocabulary.get(word)][j] = new Double(elements[j + 1]);
-                    }
-                }
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
         }
 
         for (int i = 0; i < vocabularySize; i++) {
             if (MatrixOps.absNorm(wordVectors[i]) == 0.0) {
-                System.out.println("The word \"" + id2WordVocabulary.get(i)
-                        + "\" doesn't have a corresponding vector!!!");
-                throw new Exception();
+                throw new Exception("The word \"" + id2WordVocabulary.get(i) + "\" doesn't have a corresponding vector!!!");
             }
         }
     }
